ebb68cc33837005103d306ed2d7e592da3694148
Ben <ben@notionparallax.co.uk>
2019-05-31 19:22:17 +1000

    tidy up

--- admin/ascii_faces.py
+++ admin/ascii_faces.py
@@ -3,9 +3,6 @@
 
 Run this and it will produce an HTML file with links to everyone's mugshots
 """
-
-from __future__ import division
-from __future__ import print_function
 import os
 
 LOCAL = os.path.dirname(os.path.realpath(__file__))  # the context of this file
@@ -13,7 +10,7 @@ CWD = os.getcwd()  # The curent working directory
 # print("LOCAL", LOCAL)
 # print("CWD", CWD)
 
-rootdir = '../code1161StudentRepos'
+rootdir = "../code1161StudentRepos"
 
 
 def make_ascii_faces():
@@ -30,5 +27,6 @@ def make_ascii_faces():
             print(full_command)
         except Exception as e:
             print(student_repo, e)
+
 
 make_ascii_faces()
