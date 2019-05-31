2377a70fac0238b03df9763b64ad6dace2ab756a
Ben <ben@notionparallax.co.uk>
2019-05-31 18:43:40 +1000

    modernise week 1

--- week1/tests.py
+++ week1/tests.py
@@ -4,47 +4,57 @@ import sys
 import os
 import inspect
 from pathlib import Path
+from colorama import Fore
+from colorama import Style
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from codeHelpers import completion_message
-from codeHelpers import nyan_cat
-from codeHelpers import test
-from codeHelpers import test_flake8
-from codeHelpers import ex_runs
 
-sys.path.append("../me")
+from codeHelpers import (
+    completion_message,
+    ex_runs,
+    lab_book_entry_completed,
+    loadExerciseFile,
+    nyan_cat,
+    test,
+    test_flake8,
+)
+
 
 # from codeHelpers import test
-from week1 import exercise1
+# from week1 import exercise1
 
 WEEK_NUMBER = 1
 ME = "../me"
+EM = Fore.YELLOW
+NORM = Fore.WHITE
 
 # the context of this file
-LOCAL = os.path.dirname(os.path.realpath(__file__))
-# The curent working directory
-CWD = os.getcwd()
+sys.path.append("../me/week{}".format(WEEK_NUMBER))
 
 testResults = []
 
 
 def test_hello_world():
-    source = "".join(inspect.getsourcelines(exercise1)[0]).lower()
-    if "print('hello world!')" in source or 'print("hello world!")' in source:
+    exercise1 = loadExerciseFile(weekNumber=WEEK_NUMBER, exerciseNumber=1)
+    source = "".join(inspect.getsourcelines(exercise1)[0])
+    if (
+        "print('hello world!')" in source.lower()
+        or 'print("hello world!")' in source.lower()
+    ):
         return True
-    else:
-        print(
-            """
+
+    print(
+        """
 We're looking for:
 
-print(\"hello world!\")
+{em}print(\"hello world!\"){norm}
 
-but your code is \n{sep}\n{code}\n{sep}
+but your code is \n{sep}\n{em}{code}{norm}\n{sep}
 Look carefully at your capitalisation, 
 spelling, brackets, spaces etc.""".format(
-                code=source, sep="═" * 80
-            )
+            code=source, sep="═" * 80, em=EM, norm=NORM
         )
+    )
 
     return False
 
@@ -55,26 +65,10 @@ def test_dev_env():
     else:
         print(
             "Python doesn't seem to be installed properly on your computer.\n"
-            "Have you installed Anaconda? Have you restated your computer?\n"
+            "Have you installed Anaconda?\n"
+            "Have you restarted your computer?\n"
             "Talk to a tutor and get them to help you out."
         )
-    return False
-
-
-def lab_book_entry_completed():
-    lab_book = Path(os.path.join(ME, "week1/readme.md"))
-    if lab_book.is_file():
-        with open(lab_book, "r") as f:
-            lines = f.readlines()
-            if lines == [
-                "TODO: Reflect on what you learned this week and what is still unclear.\n"
-            ]:
-                print("\nWrite something in your lab book, it'll help you think!")
-                return False
-            elif lines == [] or lines == [""]:
-                print("\nCome on, just write something, it'll help you think!")
-            elif lines:
-                return True
     return False
 
 
@@ -83,4 +77,4 @@ if __name__ == "__main__":
         test(test_dev_env(), "Python is installed and configured on this machine")
     )
     testResults.append(test(test_hello_world(), "Exercise1: Print 'Hello world!'"))
-    testResults.append(test(lab_book_entry_completed(), "Lab book entry completed"))
+    testResults.append(test(lab_book_entry_completed(1), "Lab book entry completed"))
