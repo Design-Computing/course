4b24541aef9856082a6f7031dff54ec79ab9a205
Ben <ben@notionparallax.co.uk>
2019-06-02 18:33:14 +1000

    rename to be a bat

---
+++ startup.bat
@@ -0,0 +1,25 @@
+echo Let's pip install a load of python libraries
+pip install -r requirements.txt 
+echo And now we'll install a bunch of extensions for vs code
+call code --install-extension Andreabbondanza.ignoregit
+call code --install-extension ban.spellright
+call code --install-extension CoenraadS.bracket-pair-colorizer
+call code --install-extension dbaeumer.vscode-eslint
+call code --install-extension DotJoshJohnson.xml
+call code --install-extension eamodio.gitlens
+call code --install-extension esbenp.prettier-vscode
+call code --install-extension jithurjacob.nbpreviewer
+call code --install-extension joelday.docthis
+call code --install-extension lmcarreiro.vscode-smart-column-indenter
+call code --install-extension mechatroner.rainbow-csv
+call code --install-extension ms-mssql.mssql
+call code --install-extension ms-python.anaconda-extension-pack
+call code --install-extension ms-python.python
+call code --install-extension ms-vsliveshare.vsliveshare
+call code --install-extension msjsdiag.debugger-for-chrome
+call code --install-extension Perkovec.emoji
+call code --install-extension redhat.vscode-yaml
+call code --install-extension thierrymichel.vscode-gitmoji-snippets
+call code --install-extension travisthetechie.write-good-linter
+call code --install-extension Tyriar.sort-lines
+call code --install-extension Zignd.html-css-class-completion
