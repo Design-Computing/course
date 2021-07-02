8e294972416870e4226205efc5867967b8bd61b0
Ben <ben@notionparallax.co.uk>
2021-07-02 11:20:12 +1000

    easier to read docstring

--- set4/IOexamples.py
+++ set4/IOexamples.py
@@ -2,15 +2,15 @@
 
 Using file IO, from the docs:
     "The first argument is a string containing the filename. The second
-    argument is another string containing a few characters describing the
-    way in which the file will be used. mode can be 'r' when the file will
-    only be read, 'w' for only writing (an existing file with the same name
-    will be erased), and 'a' opens the file for appending; any data written
-    to the file is automatically added to the end. 'r+' opens the file for
-    both reading and writing. The mode argument is optional; 'r' will be
-    assumed if it's omitted."
-https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+    argument is another string containing a few characters describing 
+    the way in which the file will be used. mode can be: 
+        'r' when the file will only be read, 
+        'w' for only writing (an existing file with the same name will be erased),
+        'a' opens the file for appending; any data written to the file is automatically added to the end. 
+        'r+' opens the file for both reading and writing. 
+    The mode argument is optional; 'r' will be assumed if it's omitted."
 
+https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
 
 
