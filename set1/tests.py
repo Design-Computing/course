7867134f1725f672d4ca379e727a9b9fcf9b6ad6
Ben Doherty <ben@notionparallax.co.uk>
2022-06-08 17:06:30 +1000

    bigger picture, looks better

--- set1/tests.py
+++ set1/tests.py
@@ -259,7 +259,7 @@ def has_real_photo(repo_path):
 
     if colour_count > 10:
         im = image.convert("P", palette=Image.Palette.ADAPTIVE, colors=9)
-        block_image = blocky_photo(im, width=50)
+        block_image = blocky_photo(im, width=60)
         print(block_image)
         ret_val = True
     else:
