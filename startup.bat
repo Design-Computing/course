2e76009b4af9434ff7c06daac767fd0f2f3f262f
Ben Doherty <ben@notionparallax.co.uk>
2023-05-26 16:18:40 +1000

    removed some depricated extensions
    related to #25

--- startup.bat
+++ startup.bat
@@ -4,22 +4,17 @@ pip install -r requirements.txt
 echo And now we'll install a bunch of extensions for vs code
 call code --install-extension Andreabbondanza.ignoregit
 call code --install-extension ban.spellright
-call code --install-extension CoenraadS.bracket-pair-colorizer
 call code --install-extension dbaeumer.vscode-eslint
 call code --install-extension DotJoshJohnson.xml
 call code --install-extension esbenp.prettier-vscode
-call code --install-extension jithurjacob.nbpreviewer
 call code --install-extension joelday.docthis
-call code --install-extension lmcarreiro.vscode-smart-column-indenter
 call code --install-extension mechatroner.rainbow-csv
 call code --install-extension ms-mssql.mssql
 call code --install-extension ms-python.python
 call code --install-extension ms-vsliveshare.vsliveshare
-call code --install-extension msjsdiag.debugger-for-chrome
-call code --install-extension Perkovec.emoji
 call code --install-extension redhat.vscode-yaml
-call code --install-extension thierrymichel.vscode-gitmoji-snippets
 call code --install-extension travisthetechie.write-good-linter
 call code --install-extension Tyriar.sort-lines
 call code --install-extension Zignd.html-css-class-completion
 call code --install-extension esbenp.prettier-vscode
+call code --install-extension oderwat.indent-rainbow
