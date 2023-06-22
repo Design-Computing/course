0870f70364b4815708b73e3cd279f05f6cdeac97
Ben <ben@notionparallax.co.uk>
2023-06-23 09:52:38 +1000

    Create extensions.sh

---
+++ extensions.sh
@@ -0,0 +1,15 @@
+echo "Now we'll install a bunch of extensions for vs code"
+code --install-extension Andreabbondanza.ignoregit
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension DotJoshJohnson.xml
+code --install-extension esbenp.prettier-vscode
+code --install-extension joelday.docthis
+code --install-extension mechatroner.rainbow-csv
+code --install-extension ms-mssql.mssql
+code --install-extension ms-python.python
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension redhat.vscode-yaml
+code --install-extension travisthetechie.write-good-linter
+code --install-extension Tyriar.sort-lines
+code --install-extension Zignd.html-css-class-completion
+code --install-extension oderwat.indent-rainbow
